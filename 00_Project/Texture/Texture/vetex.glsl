a3ae1f076d759fe0a8dd08b3919bca1d95ae4ef7
Rain-Bows <1075767097@qq.com>
2026-01-01 18:50:20 +0900

    Add Texture Project

---
+++ 00_Project/Texture/Texture/vetex.glsl
@@ -0,0 +1,14 @@
+﻿#version 330 core
+layout (location = 0) in vec3 position; // position变量的属性位置值为0
+layout (location = 1) in vec3 color;
+layout (location = 2) in vec2 texCoord;
+
+out vec3 vertexColor; // 为片段着色器指定一个颜色输出
+out vec2 TexCoord;
+
+
+void main(){
+    gl_Position = vec4(position, 1.0); // 注意我们如何把一个vec3作为vec4的构造器的参数
+    vertexColor = color; 
+	TexCoord = vec2(texCoord.x, 1.0f - texCoord.y);
+}
