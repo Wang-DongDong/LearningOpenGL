ddf3b333b08ec9a18679ad7dcc760b771fd18304
Rain-Bows <dongdong.wang_c@rightware.com>
2026-01-19 09:43:06 +0900

    坐标系统

---
+++ 00_Project/CoordinatedSystem/CoordinatedSystem/Shader.h
@@ -0,0 +1,23 @@
+﻿#ifndef SHADER_H
+#define SHADER_H
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+//GLEW
+#include <GL/glew.h>
+
+class Shader
+{
+public:
+	// 程序ID
+	GLuint Program;
+	// 构造器读取并构建着色器
+	Shader(const GLchar* vertexPath, const GLchar* framentPath);
+	// 使用程序
+	void Use();
+};
+
+#endif
---
+++ 00_Project/CoordinatedSystem/CoordinatedSystem/vetex.glsl
@@ -0,0 +1,17 @@
+﻿#version 330 core
+layout (location = 0) in vec3 position; // position变量的属性位置值为0
+layout (location = 1) in vec3 color;
+layout (location = 2) in vec2 texCoord;
+
+out vec3 vertexColor; // 为片段着色器指定一个颜色输出
+out vec2 TexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main(){
+    gl_Position = projection * view * model * vec4(position, 1.0f);
+    vertexColor = color; 
+	TexCoord = vec2(texCoord.x, 1.0f - texCoord.y);
+}
